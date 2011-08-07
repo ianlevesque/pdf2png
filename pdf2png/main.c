173b0c5813ba4c7e92047a989a63099bf1d7e4ad
Ian Levesque <ian@ianlevesque.org>
2011-08-07 02:56:23 -0400

    Initial Commit

---
+++ pdf2png/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  pdf2png
+//
+//  Created by Ian Levesque on 8/7/11.
+//  Copyright 2011 Ephemeron Industries. All rights reserved.
+//
+
+#include <CoreServices/CoreServices.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
